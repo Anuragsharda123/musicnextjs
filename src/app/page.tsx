04e019fed0d9d8e253283bd986694ee4ec45f0d7
Anuragsharda123 <anuragsharda131@gmail.com>
2025-01-06 23:09:57 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+'use client';
+
 import React from "react";
 import HeroSection from '@/components/HeroSection';
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpComingWebinars from "@/components/UpComingWebinars";
+import Instructors from "@/components/Instructors";
 
 const Home = () => {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection />
     <FeaturedCourses />
+    <WhyChooseUs />
+    <MusicSchoolTestimonials />
+    <UpComingWebinars />
+    <Instructors />
   </main>
       );
 }
